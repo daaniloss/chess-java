c90bdcaeea8fc6975fe6547fa4d322932a742f0d
daaniloss <dsouza.danilo@gmail.com>
2021-05-14 00:29:31 -0300

    First class: Position

--- src/Programa.java
+++ src/Programa.java
@@ -1,8 +1,10 @@
+import boardgame.Position;
 
 public class Programa {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world");
+		Position pos = new Position(3,2);
+		System.out.println(pos);
 	}
 
 }
